314e69a89ec1b68bd8cbf23bd8aaf0526d6d29e5
Doan Anh Quan <doananhquan.bdg@gmail.com>
2024-02-04 22:05:22 +0700

    super USEFUL tips!!!!11!1111!!1!

---
+++ bottomTips.js
@@ -0,0 +1,55 @@
+// <div id="copyright">
+//  <img width="30px" height="30px" src="./monster.gif" />
+//  <label>Web của Quân hehe</label>
+// </div>;
+
+// #copyright {
+//   display: flex;
+//   position: absolute;
+//   top: calc(100% - 30px);
+// }
+// #copyright img,
+// #copyright label {
+//   align-self: flex-end;
+// }
+
+var textArr = [
+  "Web của Quân hehe",
+  "Dòng chữ này sẽ thay đổi",
+  "Skill issue💀💀",
+  "Memaybeo",
+  "Alt + f4",
+  "Hehe boi",
+  "Vấn đề kĩ năng",
+  "Bingchilling",
+  "早上好中国",
+  "こんにちは",
+  "Smurf Cat",
+  "Super Secret Easter Egg!!!!",
+  "Xue hua piao piao",
+  "Super Idol de xiao rong",
+];
+var randomIndex = Math.floor(Math.random() * textArr.length);
+var randomText = textArr[randomIndex];
+document.body.innerHTML += `
+    <!-- Vấn đề kĩ năng -->
+    <div id="copyright">
+        <img width="30px" height="30px" src="https://doananhquan.github.io/images/monster.gif" />
+        <label>Tip: ${randomText}</label>
+    </div>
+    <style>
+        #copyright {
+            display: flex;
+            position: absolute;
+            top: calc(100% - 30px);
+        }
+        #copyright img,
+        #copyright label {
+            align-self: flex-end;
+        }
+        #copyright label {
+            font-family: Arial, sans-serif, "Yu Mincho", "Microsoft JhengHei"
+        }
+    </style>
+    <!-- Vấn đề về não -->
+`;
